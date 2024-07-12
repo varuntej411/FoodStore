206eba45dfdc9fdd8f07a780f3d51227d2565673
varuntej411 <derangulavaruntej@gmail.com>
2024-07-12 19:49:49 +0530

    initial commit

--- app/src/main/java/com/food/store/ui/theme/Theme.kt
+++ app/src/main/java/com/food/store/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.food.store.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
